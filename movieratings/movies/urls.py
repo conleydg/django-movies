2ca0959849ce2ff035cad773bf0046207ae42c67
David Conley <conleydg@gmail.com>
2016-05-05 15:28:42 -0400

    add

--- movieratings/movies/urls.py
+++ movieratings/movies/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^top/', views.top_twenty, name='top_twenty'),
+    url(r'^users/', views.users, name='users'),
 ]
--- movieratings/movies/views.py
+++ movieratings/movies/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Rater, Movie, Rating
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the views index.")
+
+def top_twenty(request):
+    return HttpResponse("Hello, world. You're at the top twenty view.")
+
+def users(request):
+    return HttpResponse("Hello, world. You're at the users view.")
