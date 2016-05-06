a13ada537447b05c886535331708a5de460e1401
David Conley <conleydg@gmail.com>
2016-05-05 22:38:03 -0400

    adding templates

--- movieratings/movieratings/urls.py
+++ movieratings/movieratings/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^movies/', include('movies.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^movies/', include('movies.urls')),
+
 ]
--- movieratings/movies/urls.py
+++ movieratings/movies/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^top/', views.top_twenty, name='top_twenty'),
     url(r'^users/', views.users, name='users'),
+    url(r'^(?P<movie_id>[0-9]+)/$', views.movie, name='movie'),
+    url(r'^rater/(?P<user_id>[0-9]+)/$', views.raterer, name='rater'),
 ]
